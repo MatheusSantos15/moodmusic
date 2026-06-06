c0cb4fbf1568314136523761e026dac3288d3478
Matheus Santos <104765468+MatheusSantos15@users.noreply.github.com>
2026-06-06 19:25:12 -0300

    User_Service - Básico
    Implementação das funções básicas do User_Service:
    POST
    GET
    PUT

--- user_service/app/main.py
+++ user_service/app/main.py
@@ -1,7 +1,42 @@
 from fastapi import FastAPI
+from app.schemas.user import UserCreate, MoodUpdate
 
 app = FastAPI()
+
+usuarios = []
+
+
 
 @app.get("/")
 def raiz():
     return {"mensagem": "User Service funcionando"}
+
+@app.post("/users")
+def criar_usuario(user: UserCreate):
+    
+    novo_usuario = {
+        "id": len(usuarios) + 1,
+        "nome": user.nome,
+        "humor": "neutro"
+    }
+    usuarios.append(novo_usuario)
+    return novo_usuario
+
+@app.get("/users")
+def listar_usuarios():
+    return usuarios
+
+@app.get("/users/{id}")
+def buscar_usuario(id: int):
+    for usuario in usuarios:
+        if usuario["id"] == id:
+            return usuario
+    return {"erro": "Usuário não encontrado"}
+
+@app.put("/users/{id}/mood")
+def atualizar_humor(id: int, mood: MoodUpdate):
+    for usuario in usuarios:
+        if usuario["id"] == id:
+            usuario["humor"] = mood.humor
+            return usuario
+    return {"erro": "Usuário não encontrado"}
---
+++ user_service/app/schemas/user.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    nome:str
+
+class User(BaseModel):
+    id:int
+    nome:str
+    humor:str
+
+class MoodUpdate(BaseModel):
+    humor:str
