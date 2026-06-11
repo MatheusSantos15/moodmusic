0b63fb1b0fdf8254bed52eb164880137d8d914c4
Matheus Santos <104765468+MatheusSantos15@users.noreply.github.com>
2026-06-11 20:56:30 -0300

    Integração com o Postgree e migrando o User_service

---
+++ user_service/app/database.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+def get_connection():
+    return psycopg2.connect(
+        host="Localhost",
+        database="moodmusic",
+        user="postgres",
+        password="1578648Mm"
+    )
+
+if __name__ == "__main__":
+    conn = get_connection()
+    print("Conectado com sucesso!")
+    conn.close()
--- user_service/app/main.py
+++ user_service/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.database import get_connection
 from app.schemas.user import UserCreate, MoodUpdate
 
 app = FastAPI()
@@ -13,30 +14,103 @@ def raiz():
 
 @app.post("/users")
 def criar_usuario(user: UserCreate):
+
+    conn = get_connection()
+    cursor = conn.cursor()
     
-    novo_usuario = {
-        "id": len(usuarios) + 1,
+    cursor.execute(
+        """
+        INSERT INTO users (nome, humor)
+        VALUES (%s, %s)
+        RETURNING id;
+        """,
+        (user.nome, "neutro")
+    )
+    user_id = cursor.fetchone()[0]
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return{
+        "id": user_id,
         "nome": user.nome,
         "humor": "neutro"
     }
-    usuarios.append(novo_usuario)
-    return novo_usuario
+
 
 @app.get("/users")
 def listar_usuarios():
-    return usuarios
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users ORDER BY id")
+
+    resultados = cursor.fetchall()
+    usuarios = []
+    for usuario in resultados:
+        usuarios.append({
+            "id": usuario[0],
+            "nome": usuario[1],
+            "humor": usuario[2]
+        })
+
+    cursor.close()
+    conn.close()
+    return usuarios        
+
+
 
 @app.get("/users/{id}")
 def buscar_usuario(id: int):
-    for usuario in usuarios:
-        if usuario["id"] == id:
-            return usuario
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM users WHERE id = %s",
+        (id,)
+    )
+
+    usuario = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
+    if usuario:
+        return {
+            "id": usuario[0],
+            "nome": usuario[1],
+            "humor": usuario[2]
+        }
     return {"erro": "Usuário não encontrado"}
+
 
 @app.put("/users/{id}/mood")
 def atualizar_humor(id: int, mood: MoodUpdate):
-    for usuario in usuarios:
-        if usuario["id"] == id:
-            usuario["humor"] = mood.humor
-            return usuario
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        UPDATE users
+        SET humor = %s
+        WHERE id = %s
+        RETURNING id, nome, humor
+        """,
+        (mood.humor, id)
+    )
+
+    usuario = cursor.fetchone()
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    if usuario:
+        return {
+            "id": usuario[0],
+            "nome": usuario[1],
+            "humor": usuario[2]
+        }
+
     return {"erro": "Usuário não encontrado"}
